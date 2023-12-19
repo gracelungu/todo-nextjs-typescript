592894d5eb9153c8f44c5fb3ebfe8b58e9f40628
Grace Lungu <grace.lungu.bw@gmail.com>
2023-12-19 20:05:41 +0100

    Add index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <p>Welcome to the Todo App built with Next.js and TypeScript</p>
+    </div>
+  )
+}
+
+export default Home
